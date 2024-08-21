207ba0eb8cc5cd3e48dd17a076f23fb8dfa767dd
victor <victor.fouquet@gmail.com>
2024-08-21 20:31:47 +0200

    Add score computation in individual

--- src/Individual/Individual.ts
+++ src/Individual/Individual.ts
@@ -3,12 +3,25 @@ import { IIndividual } from "./interfaces";
 
 export class Individual implements IIndividual {
     genome: IGenome;
-    fitness: number;
-    fitnessFn: (arg: any) => number;
+    fitness: number|null;
+    score:   number|null;
+    _speciesId: number|null;
 
-    constructor(genome: IGenome, fitnessFn: (arg:any) => number) {
+    fitnessFn: (arg: number[]) => number;
+
+    constructor(genome: IGenome, fitnessFn: (arg:number[]) => number) {
         this.genome = genome;
-        this.fitness = 0;
+        this.fitness = null;
+        this.score = null;
+        this._speciesId = genome.speciesId;
+
         this.fitnessFn = fitnessFn;
+    }
+
+    get speciesId(): number|null { return this.genome.speciesId; }
+
+    computeScore(arg: number[]): number {
+        this.score = this.fitnessFn(arg);
+        return this.score;
     }
 }
--- src/Individual/interfaces/IIndividual.ts
+++ src/Individual/interfaces/IIndividual.ts
@@ -2,7 +2,10 @@ import { IGenome } from "../../Genome";
 
 export interface IIndividual {
     genome:    IGenome;
-    fitness:   number;
+    fitness:   number|null;
+    score:     number|null;
+    speciesId: number|null;
 
-    fitnessFn: (arg: any) => number;
+    fitnessFn:    (arg: number[]) => number;
+    computeScore: (arg: number[]) => number;
 }
