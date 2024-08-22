a3702164a08255a60bd873d928b3885f77108469
victor <victor.fouquet@gmail.com>
2024-08-22 11:23:28 +0200

    Refactor population unit tests

--- test/Population/Population.test.ts
+++ test/Population/Population.test.ts
@@ -7,30 +7,48 @@ import { NodeVariation } from "../../src/Node";
 import { Population } from "../../src/Population";
 import { Speciation } from "../../src/Speciation";
 
+let indiv1: IIndividual;
+let indiv2: IIndividual;
+let indiv3: IIndividual;
+let indiv4: IIndividual;
+
+const fitnessFnSwitch = (indiv: IIndividual, inputs: number[]) => {
+    switch(indiv) {
+        case indiv1:
+            return 1;
+        case indiv2:
+            return 2;
+        case indiv3:
+            return 3;
+        case indiv4:
+            return 6;
+        default:
+            return 0;
+    }
+}
+
 beforeEach(() => {
     Speciation.clear();
+    Innovation.init(2,1);
+    indiv1 = new Individual(new Genome(
+        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
+        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
+    ));
+    indiv2 = new Individual(new Genome(
+        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
+        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
+    ));
+    indiv3 = new Individual(new Genome(
+        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
+        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
+    ));
+    indiv4 = new Individual(new Genome(
+        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
+        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
+    ));
 })
 
 test("Population should group individuals by species", () => {
-    Innovation.init(2,1);
-
-    const indiv1 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv2 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-    const indiv3 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv4 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-
     const fitnessFn = (indiv: IIndividual, args: number[]) => 1;
 
     const population = new Population(
@@ -45,94 +63,33 @@ test("Population should group individuals by species", () => {
 });
 
 test("Population should compute species and individuals scores", () => {
-    Innovation.init(2,1);
-
-    const indiv1 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv2 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-    const indiv3 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv4 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-
-    const fitnessFn = (indiv: IIndividual, inputs: number[]) => {
-        switch(indiv) {
-            case indiv1:
-                return 1;
-            case indiv2:
-                return 10;
-            case indiv3:
-                return 3;
-            case indiv4:
-                return 30;
-            default:
-                return 0;
-        }
-    }
     const population = new Population(
-        fitnessFn,
+        fitnessFnSwitch,
         [ indiv1, indiv2, indiv3, indiv4]
     );
 
     population.computeScores([]);
+    expect(indiv1.fitness).toBe(1);
+    expect(indiv1.adjustedFitness).toBe(0.5);
+    expect(indiv3.fitness).toBe(3);
+    expect(indiv3.adjustedFitness).toBe(1.5);
+    expect(indiv2.fitness).toBe(2);
+    expect(indiv2.adjustedFitness).toBe(1);
+    expect(indiv4.fitness).toBe(6);
+    expect(indiv4.adjustedFitness).toBe(3)
+
     // Species 1 contains 2 individuals, 1 and 3
     // score = ( (1 / 2) + (3 / 2) ) / 2
     expect(Speciation.getSpecies(1).score).toBe(1);
     // Species 1 contains 2 individuals, 1 and 3
-    // score = ( (10 / 2) + (30 / 2) ) / 2
-    expect(Speciation.getSpecies(2).score).toBe(10);
-    // Individual fitness should not have been affected
-    expect(indiv1.fitness).toBe(1);
-    expect(indiv3.fitness).toBe(3);
-    expect(indiv2.fitness).toBe(10);
-    expect(indiv4.fitness).toBe(30);
+    // score = ( (2 / 2) + (6 / 2) ) / 2
+    expect(Speciation.getSpecies(2).score).toBe(2);
+
 });
 
 test("Population should compute species and individuals scores", () => {
-    Innovation.init(2,1);
-
-    const indiv1 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv2 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-    const indiv3 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv4 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-
-    const fitnessFn = (indiv: IIndividual, inputs: number[]) => {
-        switch(indiv) {
-            case indiv1:
-                return 1;
-            case indiv2:
-                return 2;
-            case indiv3:
-                return 3;
-            case indiv4:
-                return 6;
-            default:
-                return 0;
-        }
-    }
     const population = new Population(
-        fitnessFn,
+        fitnessFnSwitch,
         [ indiv1, indiv2, indiv3, indiv4]
     );
 
@@ -153,25 +110,6 @@ test("Population should compute species and individuals scores", () => {
 });
 
 test("Population should extinct the species that didnt improve for 15 generations", () => {
-    Innovation.init(2,1);
-
-    const indiv1 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv2 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-    const indiv3 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv4 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-
     const fitnessFn = (indiv: IIndividual, inputs: number[]) => 0
     const population = new Population(
         fitnessFn,
@@ -191,25 +129,6 @@ test("Population should extinct the species that didnt improve for 15 generation
 });
 
 test("Population should extinct the species that no longer have individuals in the new generation", () => {
-    Innovation.init(2,1);
-
-    const indiv1 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv2 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-    const indiv3 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, 10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, 10))
-    ));
-    const indiv4 = new Individual(new Genome(
-        Innovation.nodes.map(n => new NodeVariation(n.id, -10)),
-        Innovation.connections.map(c => new ConnectionVariation(c.id, -10))
-    ));
-
     const fitnessFn = (indiv: IIndividual, inputs: number[]) => 0
     const population = new Population(
         fitnessFn,
